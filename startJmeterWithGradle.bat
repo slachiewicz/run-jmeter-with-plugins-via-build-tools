1ec2ea083b5b399f9772602f4ebbb25b050246cc
Jon Austen <djangofan@gmail.com>
2015-01-22 20:18:42 -0800

    Minor changes.

--- startJmeterWithGradle.bat
+++ startJmeterWithGradle.bat
@@ -1,4 +1,9 @@
 @ECHO off
+SET WINDOW_TITLE=Jmeter via Gradle launcher
+TITLE=%WINDOW_TITLE%
+ECHO Closing this window will close Jmeter...
+IF NOT DEFINED GRADLE_HOME ECHO It appears you are missing the Gradle installation on your system.
+IF NOT DEFINED JAVA_HOME ECHO You need to define JAVA_HOME on your system.
 
 gradle jmeterEditor %1 %2 %3
 
--- startJmeterWithMaven.bat
+++ startJmeterWithMaven.bat
@@ -2,6 +2,9 @@
 SET WINDOW_TITLE=Jmeter via Maven launcher
 TITLE=%WINDOW_TITLE%
 ECHO Closing this window will close Jmeter...
+IF NOT DEFINED M2_HOME ECHO It appears you are missing the Maven installation on your system.
+IF NOT DEFINED JAVA_HOME ECHO You need to define JAVA_HOME on your system.
+
 call setJavaHome.bat
 mvn.bat jmeter:gui -X
 
